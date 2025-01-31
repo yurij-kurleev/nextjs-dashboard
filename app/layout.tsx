9bfa6ede0aa70724e2169be9dfb3c8e7e2949be1
Yurii Kurleiev <ykurleev88@gmail.com>
2025-01-31 16:50:45 +0200

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
+import { ReactNode } from 'react';
+
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
